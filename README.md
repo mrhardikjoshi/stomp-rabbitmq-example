69706e48ee4414332612eb4633fced2b0677bc28
Hardik Joshi <hardikjoshi1991@gmail.com>
2021-10-15 02:22:46 +0530

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # stomp-rabbitmq-example
+
+# Steps to run
+1) Install and Make sure you have rabbitmq running
+2) `gem install bunny`
+3) enable rabbitmq plugin `rabbitmq-plugins enable rabbitmq_web_stomp`
+4) start file_server by `ruby server.rb`
+5) open js_stomp.html in browser and wait for approx 5 second to see the popup with response.
+
+# How this works:
+1) ruby script (server.rb) subscribes to `rpc_queue` queue and waits
+2) html page (js_stomp.html) when opened in browser sends message to `rpc_queue` queue. with `reply-to` header containing reply channel name
+3) html page then and subscribes to that `reply-to` channel and waits to receive a file.
+3) ruby script server gets the message and responds with file (test.csv) as a string on channel specified in `reply-to` header.
+4) html page receives the message and shows the alert box with all message details.
+
+# Default config used:
+Endpoint: `ws://localhost:15674/ws`
+login: `guest` 
+passcode: `guest`
---
+++ server.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+require 'bunny'
+
+class Server
+  FILENAME = 'test.csv'
+
+  def initialize
+    @connection = Bunny.new
+    @connection.start
+    @channel = @connection.create_channel
+  end
+
+  def start(queue_name)
+    @queue = channel.queue(queue_name)
+    @exchange = channel.default_exchange
+    subscribe_to_queue
+  end
+
+  def stop
+    channel.close
+    connection.close
+  end
+
+  def loop_forever
+    loop { sleep 5 }
+  end
+
+  private
+
+  attr_reader :channel, :exchange, :queue, :connection
+
+  def subscribe_to_queue
+    queue.subscribe do |_delivery_info, properties, payload|
+
+      puts "got call with #{properties} /n #{payload}"
+      exchange.publish(
+        result.to_s,
+        routing_key: properties.reply_to
+      )
+    end
+  end
+
+  def result
+    return text = File.read('test.csv')
+  end
+end
+
+begin
+  server = Server.new
+
+  puts ' [x] Awaiting RPC requests'
+  server.start('rpc_queue')
+  server.loop_forever
+rescue Interrupt => _
+  server.stop
+end
